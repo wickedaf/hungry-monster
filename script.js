98facf1ee970e02f97e2c3efec34f3c0382f4a19
Shahriar Emon <rd.emon2@gmail.com>
2021-02-08 22:14:26 +0600

    Changed Bangla loop to foreach

--- script.js
+++ script.js
@@ -3,52 +3,61 @@ document.getElementById("submit").addEventListener("click", () => {
   fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchQuery)
     .then((response) => response.json())
     .then((data) => setMeal(data)); // Return one object with meals array
-    document.getElementById("meal-card").innerHTML = " ";
-    document.getElementById("info-area").innerHTML = " ";
-
+  document.getElementById("meal-card").innerHTML = " ";
+  document.getElementById("info-area").innerHTML = " ";
+  document.getElementById("meal-card").style.display = "block";
 });
 
 function setMeal(mealsObj) {
   const getMeal = mealsObj.meals;
   const mealCard = document.getElementById("meal-card");
-  for (let i = 0; i < getMeal.length; i++) {
-    let mealCardSingleId = `meal-card-single${i}`;
+
+  //getMeal Array Loop
+  getMeal.forEach((element) => {
+    let mealCardSingleId = `meal-card-single${element.idMeal}`;
     let cardElement = `
-          <img class="card-img-top pt-2" src="${getMeal[i].strMealThumb}" alt="Card image cap">
+          <img class="card-img-top pt-2" src="${element.strMealThumb}" alt="Card image cap">
           <div class="card-body">
-              <p id="meal-name" class="card-text text-center">${getMeal[i].strMeal}</p>
+              <p id="meal-name" class="card-text text-center">${element.strMeal}</p>
           </div>
     `;
     const div = document.createElement("div");
     div.setAttribute("class", "card m-2");
-    div.setAttribute("id", mealCardSingleId)
+    div.setAttribute("id", mealCardSingleId);
     div.setAttribute("style", "width: 13rem");
     div.innerHTML = cardElement;
     mealCard.appendChild(div);
+
+    //Card Click Handler
     document.getElementById(mealCardSingleId).onclick = () => {
-      let infoAreaElement = `Ingredients - 
-      ${getMeal[i].strIngredient1}|
-      ${getMeal[i].strIngredient2}|
-      ${getMeal[i].strIngredient3}|
-      ${getMeal[i].strIngredient4}|
-      ${getMeal[i].strIngredient5}|
-      ${getMeal[i].strIngredient6}|
-      ${getMeal[i].strIngredient7}|
-      ${getMeal[i].strIngredient8}|
-      ${getMeal[i].strIngredient9}|
-      ${getMeal[i].strIngredient10}|
-      ${getMeal[i].strIngredient11}|
-      ${getMeal[i].strIngredient12}|
-      ${getMeal[i].strIngredient13}|
-      ${getMeal[i].strIngredient14}
-      ` 
+      let infoAreaElement = `
+      <div class="card" style="width: 28rem;">
+        <img src="${element.strMealThumb}" class="card-img-top pt-2" alt="...">
+        <div class="card-body">
+          <p class="card-text">Ingredients - 
+          <ol>
+          <li>${element.strIngredient1} - ${element.strMeasure1}</li>
+          <li>${element.strIngredient2} - ${element.strMeasure2}</li>
+          <li>${element.strIngredient3} - ${element.strMeasure3}</li>
+          <li>${element.strIngredient4} - ${element.strMeasure4}</li>
+          <li>${element.strIngredient5} - ${element.strMeasure5}</li>
+          <li>${element.strIngredient6} - ${element.strMeasure6}</li>
+          <li>${element.strIngredient7} - ${element.strMeasure7}</li>
+          <li>${element.strIngredient8} - ${element.strMeasure8}</li>
+          <li>${element.strIngredient9} - ${element.strMeasure9}</li>
+          <li>${element.strIngredient10} - ${element.strMeasure10}</li>
+          <li>${element.strIngredient11} - ${element.strMeasure11}</li>
+          <li>${element.strIngredient12} - ${element.strMeasure12}</li>
+          <li>${element.strIngredient13} - ${element.strMeasure13}</li>
+          <li>${element.strIngredient14} - ${element.strMeasure14}</li>
+          </ol>
+          </p>
+        </div>
+      </div>
+      `;
+      document.getElementById("info-area").style.display = "block";
       document.getElementById("info-area").innerHTML = infoAreaElement;
-    }
-  }
+      document.getElementById("meal-card").innerHTML = " ";
+    };
+  });
 }
-
-// const mealCardSingle = document.getElementById("meal-card-single");
-// mealCardSingle.onclick = () => {
-
-// }
-
